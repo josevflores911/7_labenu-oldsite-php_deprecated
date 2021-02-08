703afc2c1b570558b2fff60ab883db60aa4bbf7b
josevflores911 <59713685+josevflores911@users.noreply.github.com>
2021-02-08 16:19:27 -0300

    crud from youtube working on php

---
+++ crud1/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD PHP</title>
+
+		<!-- CSS only -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</head>
+<body>
+	
+	<?php require_once 'process.php'; ?>
+
+	<?php if(isset($_SESSION['message'])): ?>
+		<div class="alert alert-<?=$_SESSION['msg_type']?>">
+			<?php
+				echo $_SESSION['message'];
+				unset($_SESSION['message']);
+			?>
+		</div>
+	<?php endif ?>
+	<div class="container">
+	<?php
+		$mysqli = new mysqli('localhost','root','','crud') or die(mysqli_error($mysqli));
+		$result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+		//THIS IS A COMMENT
+		//pre_r($result);
+		//pre_r($result->fetch_assoc());
+		
+		?>
+
+		<div class="row justify-content-center">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Location</th>
+						<th colspan="2">Action</th>
+
+					</tr>
+				</thead>
+
+			<?php
+				while($row = $result->fetch_assoc()): ?>
+			<tr>
+				<td> <?php echo $row['name'];?> </td>
+				<td> <?php echo $row['location'];?>	</td>
+				<td>
+					<a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-info"> Edit</a>
+					<a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+
+				</td>
+			</tr>
+			<?php endwhile; ?>
+			</table>
+		</div>
+		<?php
+			function pre_r( $array ) {
+				echo '<pre>';
+				print_r($array);
+				echo '</pre>';
+
+			}
+
+		?>
+		<div class="row justify-content-center">
+			<form action="process.php" method="POST">
+				<input type="hidden" name="id" value="<?php echo $id;?>">
+				<div class="form-group">
+				<label>Name</label>
+				<input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="enter your name">
+				</div>
+				<div class="form-group">
+				<label>Location</label>
+				<input type="text" name="location" value="<?php echo $location; ?>" class="form-control" placeholder= "enter your location">
+				</div>
+				<div class="form-group">
+					<?php
+						if ($update ==true):
+					?>	
+
+					<button type="submit" class="btn btn-info" name="update">Update</button>
+
+					<?php else: ?>
+					<button type="submit" class="btn btn-primary" name="save">Save</button>
+				<?php endif; ?>
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
+
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
---
+++ crud1/readme.txt
@@ -0,0 +1,5 @@
+taked from youtube
+site: https://www.youtube.com/watch?v=3xRMUDC74Cw
+anexar base de dato y tablas
+bootstrap from internet
+working fine!!
